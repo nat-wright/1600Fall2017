592cd8f00dcc6d9f975a520f904eeaa0a056a445
Nat Wright <natalee.wright413@gmail.com>
2017-10-25 11:51:13 -0600

    HealthBarPt2

--- 1600Fall2017/Assets/Scripts/UI/UIBar.cs
+++ 1600Fall2017/Assets/Scripts/UI/UIBar.cs
@@ -7,17 +7,47 @@ public class UIBar : MonoBehaviour {
 
 	public Image bar;
 	public float barTime = 0.1f;
+	public float powerLevel = 0.1f;
+	public float ammountToAdd = 0.01f;
 
-	// Use this for initialization
-	void OnTriggerEnter () {
-		StartCoroutine(UpdateBar());
+	public enum PowerUpType
+	{
+		PowerUp,
+		PowerDown
 	}
 
-	IEnumerator UpdateBar () {
-		while (bar.fillAmount > 1)
+	public PowerUpType powerUp;
+
+	void OnTriggerEnter () {
+
+		switch (powerUp)
 		{
-			bar.fillAmount += barTime;
+			case PowerUpType.PowerUp:
+				StartCoroutine(PowerUpBar());
+			break;
+
+			case PowerUpType.PowerDown:
+				StartCoroutine(PowerDownBar());
+			break;
+		}
+	}
+
+
+	IEnumerator PowerUpBar () {
+		while (bar.fillAmmount < 1)
+		{
+			bar.fillAmmount += ammountToAdd;
 			yield return new WaitForSeconds(barTime);
 		}
 	}
+	IEnumerator PowerDownBar () {
+		float tempAmmount = powerLevel;
+		while (tempAmmount > 0)
+		{
+			tempAmmount -= ammountToAdd;
+			bar.fillAmmount -= tempAmmount;
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+
 }
